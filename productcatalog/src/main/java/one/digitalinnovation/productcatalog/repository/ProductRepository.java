cda9be3e9f535a3f7194aa178e729d0293608521
Jose Pereira <josepsneto@gmail.com>
2021-08-21 19:53:18 -0300

    Creates the whole project from the DIO Bootcamp

---
+++ productcatalog/src/main/java/one/digitalinnovation/productcatalog/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.productcatalog.repository;
+
+import one.digitalinnovation.productcatalog.model.Product;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface ProductRepository extends CrudRepository<Product, Integer> {
+
+
+
+
+
+}
---
+++ service-discovery/src/test/java/one/innovation/digital/servicediscovery/ServiceDicoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package one.innovation.digital.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDicoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
